01738e8d10729370eaabd78df970f856e2e58523
imagine27cm-code <imagine27cm-code@users.noreply.github.com>
2026-04-20 00:22:59 +0500

    fix: обновил Prisma 7 с официальным драйвером адаптером pg

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,11 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
+import { Pool } from 'pg'
+import { PrismaPg } from '@prisma/adapter-pg'
+
+const connectionString = process.env.DATABASE_URL!
+
+const pool = new Pool({ connectionString })
+const adapter = new PrismaPg(pool)
 
 declare global {
   var __prisma: PrismaClient | undefined;
@@ -7,8 +14,8 @@ declare global {
 export const prisma =
   globalThis.__prisma ??
   new PrismaClient({
+    adapter,
     log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
   });
 
 if (process.env.NODE_ENV !== "production") globalThis.__prisma = prisma;
-
